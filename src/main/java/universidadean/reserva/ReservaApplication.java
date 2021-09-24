c0fa28afa4e6f40f3d71c6a4240c6c2f979e4b7b
nmantill9847 <89228492+nmantill9847@users.noreply.github.com>
2021-09-24 16:13:46 -0500

    Diseño Home y reservas

--- src/main/java/universidadean/reserva/ReservaApplication.java
+++ src/main/java/universidadean/reserva/ReservaApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ReservaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ReservaApplication.class, args);
+	public static void main(String[] args) {SpringApplication.run(ReservaApplication.class, args);
 	}
 
 }
+
